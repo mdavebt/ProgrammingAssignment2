d755b93c725b88592afcec9269e26a9eaec9dba0
mdavebt <mdavebt@gmail.com>
2018-06-22 12:05:45 +0530

    Assignment 2 Submission 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## x is a vector
+## nr = number of rows, nc = number of columns, nr = nc for square matrix
+## m is the cached matrix
+## invm is the inverse of cached matrix
+## makeCacheMatrix will put all the objects in global enviornment
+## Thus we can use all its objects for cacheSolve
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = vector(), nr, nc=nr) {
 
-makeCacheMatrix <- function(x = matrix()) {
+       	y <- NULL
+	
+	sety <- function(){
+		y <<- x
+		y
+	}
 
+	if(!is.null(sety())){
+		y <<- x
+	}
+
+	m <<- matrix(y,nr)
+
+	invm <<- solve(m)
 }
 
+## This function will solve for the matrix developed in above function
+## newy = new vector supplied of new matrix
+## newr = rows of new vector, newc = columns of new vector
+## newm = new matrix which is generated for comparision is cache matrix
+## This fucntion solves new matrix only if the newm is different from m
 
-## Write a short comment describing this function
+cacheSolve <- function(newy = vector(), newr, newc = newr) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+	newm <- matrix(newy, newr, newc)
+
+	nr <- nrow(m)
+
+	if((length(setdiff(y,newy)) == 0) 
+	&& (length(setdiff(newy,y)) == 0)
+	&& (nr == newr)){
+		message("Using Cache Matrix Inverse")
+		return(invm)
+	} else {
+		newinvm <- solve(newm)
+		newinvm
+	}
+		
+ 
+ }
